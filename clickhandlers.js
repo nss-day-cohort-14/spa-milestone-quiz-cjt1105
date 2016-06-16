cc506075d455555cc6401c81bf76f37acfbb9083
CJT1105 <CJT1105@GMAIL.COM>
2016-06-16 09:34:24 -0500

    got lot to populate

---
+++ clickhandlers.js
@@ -0,0 +1,22 @@
+var CarLot =(function(orginal){
+	orginal.clickedCar = function(color){
+		event.currentTarget.classList.add('clickedCar');
+		event.currentTarget.style.backgroundColor = color
+	};
+	orginal.removeClass = function(){
+		event.currentTarget.classList.remove('clickedCar');
+		event.currentTarget.style.backgroundColor = "white"
+	}
+	// return {
+ //    clickedCar: function(color) {
+ //      event.currentTarget.classList.add('clickedCar');
+	// 		event.currentTarget.style.backgroundColor = color
+ //    },
+ //    removeClass: function(){
+ //    	event.currentTarget.classList.remove('clickedCar');
+	// 		event.currentTarget.style.backgroundColor = "white"
+ //    }
+ //  }
+	
+	return orginal
+})(CarLot)
---
+++ populateLot.js
@@ -0,0 +1,28 @@
+Carlot = (function(originalFunction){
+  originalFunction.activeEvents = function(){
+    carContainer = document.querySelectorAll('.car-container');
+    for ( let i = 0; i < carContainer.length; i ++){
+      carContainer[i].addEventListener('click', function(){
+        if(event.currentTarget.classList[1] === "clickedCar"){
+          originalFunction.removeClass();
+          console.log("target",event.currentTarget );
+        }
+				else {
+          originalFunction.clickedCar("red");
+        }
+      })
+    }
+    userInput.addEventListener("keyup", function(e){
+			if(e.keyCode === 13){
+				input = userInput.value;
+				alert('hi');
+			}
+    })
+  }
+
+})(CarLot)
+
+
+
+
+
